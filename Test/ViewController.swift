5d8cceb1f0ed4126a8ab3dabc0cd1a5d7a25ab53
YukiFuruhashi <yukifuruhashi@icloud.com>
2024-03-02 08:24:44 +0900

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by yukifuruhashi on 2024/03/02.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
